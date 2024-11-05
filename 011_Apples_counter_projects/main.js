4b8e33fc12f510a36e6aaf0b50ca28cf89ec69cf
Md Azizul Haque <khanazizul1199@gmail.com>
2024-11-05 21:48:39 +0600

    children props

--- 011_Apples_counter_projects/main.js
+++ 011_Apples_counter_projects/main.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import { AppleCounter } from "./components/AppleCounter";
 import Counter from "./src/react-state/Counter";
 import HowStateWorks from "./src/how_does_state_works/HowStateWorks";
+import ChildrenProps from "./src/children_prop/ChildrenProps";
 
 export default function App() {
 	return (
 		<>
+			<ChildrenProps>
+				<h1>This children of ChildrenProps component</h1>
+			</ChildrenProps>
 			<HowStateWorks />
 			<Counter />
 			<AppleCounter></AppleCounter>
